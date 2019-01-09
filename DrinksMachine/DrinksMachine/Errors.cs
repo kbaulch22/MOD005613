45fbda3fb1be0b2829bd6106e3c8564eab19be19
kbaulch22 <46493669+kbaulch22@users.noreply.github.com>
2019-01-09 15:41:09 +0000

    Added Drinks Machine code solution

---
+++ DrinksMachine/DrinksMachine/Errors.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace DrinksMachine
+{
+    class Errors
+    {
+        public static void InvalidOption()
+        {
+            Console.Write("Invalid option. Please input a single digit number and press enter/return to continue.");
+            Console.ReadKey();
+        }
+        public static void OutOfStock(bool repeat_loop)
+        {
+            // displays error message to user
+            Console.WriteLine("ERROR: Sorry, ingredients needed to make this drink are not in stock.\nPlease wait for machine to be restocked before ordering again.");
+            Thread.Sleep(3000);
+            // breaks loop to ensure program doesn't consume ingredients or dispense a drink
+            repeat_loop = false;
+        }
+    }
+}
